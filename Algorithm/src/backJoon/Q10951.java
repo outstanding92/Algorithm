243c46cdb84b49d2597fc841071e1371c145b622
mjjun <mjjun@DESKTOP-J7S5FEK>
2019-09-01 20:52:54 +0900

    Backjoon Q10951

---
+++ Algorithm/src/backJoon/Q10951.java
@@ -0,0 +1,23 @@
+package backJoon;
+
+import java.io.*;
+import java.util.*;
+
+
+public class Q10951 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			System.out.println(a+b);
+		}
+		sc.close();
+
+	}
+
+}
